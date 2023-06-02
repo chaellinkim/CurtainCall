591c7e4535767e25a13ce0b1901c586e1ab849a8
clin0927 <clin0927@naver.com>
2023-06-02 11:26:50 +0900

    actorlist update

---
+++ src/main/java/com/cc/model/controller/ActorController.java
@@ -0,0 +1,27 @@
+package com.cc.model.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cc.model.entity.Actor;
+import com.cc.model.service.ActorService;
+
+@Controller
+public class ActorController {
+	@Autowired
+	private ActorService actorService;
+	
+	@RequestMapping("/actor")
+	public String actorList(Model model) {
+		List<Actor> list = actorService.selectAll();
+		
+		model.addAttribute(list);
+		System.out.println("------");
+		System.out.println(list.get(0));
+		return "actorlist";
+	}
+}
