948bd6b4b73e5a88154fc51eb4d3a942d8a6afdd
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2023-06-16 11:59:30 +0900

    로그인, 회원가입 controller

--- src/main/java/com/cc/model/controller/IndexContoller.java
+++ src/main/java/com/cc/model/controller/IndexContoller.java
@@ -1,130 +1,152 @@
 package com.cc.model.controller;
 
-import java.io.ByteArrayInputStream;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import javax.servlet.http.HttpSession;
 
-import org.json.JSONObject;
-import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.cc.model.dto.PlayDto;
-import com.cc.model.service.PlayService;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.cc.model.cypher.AES256;
+import com.cc.model.cypher.SHA256;
+import com.cc.model.dto.User;
+import com.cc.model.repository.UserRepository;
+import com.cc.model.service.UserService;
 
 @Controller
-public class IndexContoller {
+public class IndexController {
+
 	@Autowired
-    private PlayService playService;
+	private UserService userSvc;
 	
-	@RequestMapping("/list")
-	public String list(Model model) {
-        String url = "http://www.kopis.or.kr/openApi/restful/pblprfr";
-        String serviceKey = "722233b68ffa4883ae5213ccf16565a5";
-        String stDate = "20230101";
-        String edDate = "20230630";
-        String rows = "10";
-        String cPage = "1";
-        String name="PF132236";
-
-        String apiUrl = url + "?service=" + serviceKey + "&stdate=" + stDate + "&eddate=" + edDate + "&rows=" + rows + "&cpage=" + cPage;
-        //String apiUrl = url +"/"+name+"?service=" + serviceKey;
-        
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response;
-        PlayDto playDto = null;
-        
-        try {
-            response = restTemplate.getForEntity(apiUrl, String.class);
-            if (response.getStatusCode() == HttpStatus.OK) {
-                String xmlResponse = response.getBody();
-                System.out.println(xmlResponse);
-
-                try {
-                    // 새로운 DocumentBuilderFactory 생성
-                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
-                    // 새로운 DocumentBuilder 생성
-                    DocumentBuilder builder = factory.newDocumentBuilder();
-
-                    // XML 응답을 Document 객체로 파싱합니다.
-                    Document document = builder.parse(new ByteArrayInputStream(xmlResponse.getBytes()));
-
-                    // "db" 요소의 목록을 가져옵니다.
-                    NodeList dbList = document.getElementsByTagName("db");
-
-                    // "db" 요소를 순회하면서 "mt20id" 태그의 값을 읽어옵니다.
-                    for (int i = 0; i < dbList.getLength(); i++) {
-                        Element dbElement = (Element) dbList.item(i);
-                        String mt20id = dbElement.getElementsByTagName("mt20id").item(0).getTextContent();
-                        System.out.println(mt20id);
-                       
-                        String apiUrl2 = url +"/"+mt20id+"?service=" + serviceKey;
-                        
-                        RestTemplate restTemplate2 = new RestTemplate();
-                        ResponseEntity<String> response2;
-                        try{
-                        	response2 = restTemplate2.getForEntity(apiUrl2, String.class);
-                        	if (response2.getStatusCode() == HttpStatus.OK) {
-                                String xmlResponse2 = response2.getBody();
-                                System.out.println(xmlResponse2);
-                                JSONObject jsonObject = XML.toJSONObject(xmlResponse2);
-                                System.out.println("json 출력"+jsonObject.toString());
-                                
-                                String jsonEx = "{\"id\":1,\"prfnm\":\"타이틀\",\"pcseguidance\":\"공연명 \",\"pcseguidance\":\"가격 \",\"prfpdfrom\":\"2023.06.10\",\"prfpdto\":\"2023.06.10\",\"prfage\":\"나이 \",\"poster\":\"링크 \",\"mt10id\":\"placeid\"}";
-                                ObjectMapper objectMapper = new ObjectMapper();
-//                              System.out.println("jsonEx : " + jsonEx);
-//                              playDto = objectMapper.readValue(jsonEx, PlayDto.class);
-                                
-                                JsonNode rootNode = objectMapper.readTree(jsonObject.toString());
-                                JsonNode dbNode = rootNode.path("dbs").path("db");
-                                playDto = objectMapper.readValue(dbNode.toString(), PlayDto.class);
-                                
-                                playService.insert(playDto);
-                                
-                                System.out.println("-----");
-                                System.out.println("playdto 출력!!!!"+playDto.toString());
-                                
-                             // 필요한 태그의 값을 가져옵니다.
-                             // String prfnm = jsonObject.getJSONObject("db").getString("prfnm");
-                        	}else {
-                        		System.out.println("통신 실패-: " + response2.getStatusCode());
-                        	}
-                        	
-                        } catch (Exception e) {
-                        	e.printStackTrace();
-                            System.out.println("통신 실패__: " + e.getMessage());
-                            
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                
-            } else {
-                System.out.println("통신 실패: " + response.getStatusCode());
-                
-            }
-        } catch (Exception e) {
-            System.out.println("통신 실패: " + e.getMessage());
-            
-        }
-        
-        model.addAttribute("test",playDto);
-        return "playlist";
-       
-    }
+	@Autowired
+	private UserRepository userRep;
 	
+	AES256 aes256 = new AES256();
+	SHA256 sha256 = new SHA256();
+
+	@RequestMapping("/login")
+	public String login(HttpSession session) {
+
+		if (session.getAttribute("name") != null) {
+			session.removeAttribute("name");
+		}
+
+		if (session.getAttribute("email") != null) {
+			session.removeAttribute("email");
+		}
+
+		//회원가입 시에 필요한 정보
+		session.setAttribute("logintype", "local");
+
+		return "login";
+	}
+
+	//로그인
+	@PostMapping("/logincheck")
+	public String logincheck(HttpSession session, Model model, String id, String password) throws NoSuchAlgorithmException {
+		// 여기에 아이디랑 비밀번호 확인해주고, db에 저장된 정보랑 일치하면
+		// 세션에 사용자 식별자 넣어주기(user_id)
+		
+		String url = "";
+		int user_id = 0;
+		
+		Optional<User> user = userSvc.loginfindid(id);
+		
+		//id가 db에 존재하는지 먼저 확인
+		if(user.isEmpty()) {
+				model.addAttribute("msg", "일치하는 회원정보가 없습니다.");
+				return "login";
+		}else {
+			String password_encode = sha256.encrypt(password);
+			if(user.orElse(new User()).getUser_passwordcypher().equals(password_encode)) {
+				//비밀번호 일치하는 경우
+				user_id = user.orElse(new User()).getUser_id();
+				session.setAttribute("user_id", user_id); //식별자 저장(사용자 정보 불러오기)
+				session.setAttribute("user_state", "login"); //로그인이 된다면 user_state에 login이라는 상태 넣어주기
+				session.setAttribute("password_encode", password_encode); //암호화된 비밀번호 --> 마이페이지에서 사용하려공 
+				model.addAttribute("user_id", session.getAttribute("user_id"));
+				return "redirect:/";
+			}else {
+				//비밀번호 일치하지 않는 경우 
+				model.addAttribute("msg", "아이디 또는 비밀번호를 확인해주세요");
+				return "login";
+			}
+		}
+	}
+
+	//회원가입 페이지로 넘어가기
+	@RequestMapping("/join")
+	public String join(Model model, HttpSession session) {
+		// name과 email 값을 사용하여 처리
+		model.addAttribute("name", session.getAttribute("name"));
+		model.addAttribute("email", session.getAttribute("email"));
+		model.addAttribute("logintype", session.getAttribute("logintype"));
+		return "join";
+	}
+
+	//회원정보 저장
+	//저장하기 전에 해줘야 할 것 --> 암호화, 이름 마스킹
+	@PostMapping("/joincheck")
+	public String joincheck(String login, String userId, String userPw, String userName, String rrn, String gender, String email) throws Exception {
+		
+		//암복호화
+		String idcypher =  aes256.encrypt(userId);
+		String namecypher = aes256.encrypt(userName);
+		String rrncypher = aes256.encrypt(rrn);
+		String emailcypher = aes256.encrypt(email);
+		
+		//암호화
+		String passwordcypher = sha256.encrypt(userPw);
+		
+		//마스킹
+		String maskingName = Masking(userName);
+		
+		if(userSvc.userInsert(userId, idcypher, userPw, passwordcypher, userName, namecypher, 
+				maskingName, rrn, rrncypher, gender,email, emailcypher, login)) {
+			
+		}
+		
+//		userRep.userInsert(userId, idcypher, userPw, passwordcypher, userName, namecypher, maskingName, rrn, rrncypher, email, emailcypher, login);
+		
+		return "redirect:/";
+	}
+	
+	
+	//회원정보 이름 마스킹
+	public String Masking(String name) {
+		
+		String frsName = name.substring(0,1);
+		String midName = name.substring(1,name.length()-1);
+		String lstName = name.substring(name.length()-1, name.length());
+		String maskingMid = "";
+		
+		for(int i = 0; i<midName.length(); i++) {
+			maskingMid += "*";
+		}
+		
+		return frsName+maskingMid+lstName ;
+	}
+
+	//아이디 중복체크
+	@ResponseBody
+	@PostMapping("/idcheck")
+	public int idcheck(String id) {
+		int result;
+		
+		if(userSvc.loginidCheck(id)) {
+			result = 1; //사용 불가
+		}else {
+			result = 0; //사용 가능
+		}
+		System.out.println(result);
+		
+		return result;
+	}
+
 }
