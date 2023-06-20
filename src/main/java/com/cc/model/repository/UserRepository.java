f68c9bc3c3103d09d4dd2ecc612f835b6a11982e
clin0927 <clin0927@naver.com>
2023-06-20 09:53:12 +0900

    user update

--- src/main/java/com/cc/model/repository/UserRepository.java
+++ src/main/java/com/cc/model/repository/UserRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cc.model.dto.User;
+import com.cc.model.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
